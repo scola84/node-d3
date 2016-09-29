7bc9993936d7eb21f2a5ecff2b06b722c8feef40
M.C. van den Boogaart <scola@kvdb.name>
2016-09-29 23:07:56 +0200

    Update scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,25 @@
 import buble from 'rollup-plugin-buble';
+import builtins from 'rollup-plugin-node-builtins';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
+  dest: './dist/d3.js',
   entry: 'index.js',
   format: 'umd',
+  moduleName: 'd3',
+  external: [
+    'moment',
+    'moment-timezone'
+  ],
   globals: {
-    'd3-selection': 'd3',
-    'd3-transition': 'd3',
     'moment': 'moment',
     'moment-timezone': 'moment'
   },
   plugins: [
+    builtins(),
     resolve({
-      jsnext: true,
-      skip: [
-        'd3-selection',
-        'd3-transition',
-        'moment',
-        'moment-timezone'
-      ]
+      jsnext: true
     }),
     commonjs({
       exclude: ['**/lodash-es/**']
