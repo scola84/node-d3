ef01cf757f901eb842d5e35e4b2519601d8336bd
M.C. van den Boogaart <scola@kvdb.name>
2017-02-21 14:01:16 +0100

    Process CSS imports

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import buble from 'rollup-plugin-buble';
 import builtins from 'rollup-plugin-node-builtins';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
 
 const name = process.argv.indexOf('-w') === -1 ?
   'd3.js' :
@@ -26,6 +27,7 @@ export default {
   },
   plugins: [
     builtins(),
+    postcss(),
     resolve({
       jsnext: true
     }),
