5ac9a4e8a364907108c12929def85d064b9a5646
M.C. van den Boogaart <scola@kvdb.name>
2017-05-07 16:19:59 +0200

    Export CSS to separate stylesheet

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import builtins from 'rollup-plugin-node-builtins';
 import commonjs from 'rollup-plugin-commonjs';
 import globals from 'rollup-plugin-node-globals';
 import resolve from 'rollup-plugin-node-resolve';
-import postcss from 'rollup-plugin-postcss';
+import css from 'rollup-plugin-css-only';
 
 const name = process.argv.indexOf('-w') === -1 ?
   'd3.js' :
@@ -28,7 +28,7 @@ export default {
   },
   plugins: [
     builtins(),
-    postcss(),
+    css(),
     resolve({
       jsnext: true
     }),
